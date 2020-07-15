df69b557fa358355ea837ebafc2a30ffd9fdd4c2
xiong <apple@Jokers-Mac-mini.local>
2020-07-15 14:00:48 +0800

    服务器被黑，数据恢复

--- lib/common/values/server.dart
+++ lib/common/values/server.dart
@@ -1,3 +1,3 @@
 // const String SERVER_API_URL = 'http://127.0.0.1:3000/mock/11';
 // const String SERVER_API_URL = 'http://119.28.233.248:40001/mock/11';
-const String SERVER_API_URL = 'http://13.250.22.15:3000/mock/11';
+const String SERVER_API_URL = 'http://13.250.22.15:3000/mock/10';
